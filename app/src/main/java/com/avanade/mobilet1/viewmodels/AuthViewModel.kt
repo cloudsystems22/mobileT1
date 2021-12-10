fa9d03e77d62bfd321659517025c93dcd2304169
cloudsystems22 <cloudsystems22@gmail.com>
2021-12-09 21:52:23 -0300

    Resolução de bug

--- app/src/main/java/com/avanade/mobilet1/viewmodels/AuthViewModel.kt
+++ app/src/main/java/com/avanade/mobilet1/viewmodels/AuthViewModel.kt
@@ -1,19 +1,20 @@
 package com.avanade.mobilet1.viewmodels
 
+import android.app.Application
+import android.content.Intent
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
 import com.avanade.mobilet1.repositories.AuthRepository
-import com.avanade.mobilet1.utils.FirebaseUtils
-import com.avanade.mobilet1.extensions.Extensions.startNewActivity
-import com.avanade.mobilet1.views.CreateAccountActivity
 import com.avanade.mobilet1.views.HomeActivity
 
-
-class AuthViewModel()
+class AuthViewModel(application: Application)
     : ViewModel() {
 
-    private val authRepository = AuthRepository()
+    var authRepository = AuthRepository(application)
+
 
     fun register(userName:String, password:String) = authRepository.register(userName, password)
 
     fun login(userName:String, password:String) = authRepository.login(userName, password)
+
 }
